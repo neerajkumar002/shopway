a146467c36b56ef87b0319b406406b8f90862c18
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-17 12:25:45 +0530

    update component name

--- src/pages/products/ProductListing.jsx
+++ src/pages/products/ProductListing.jsx
@@ -2,8 +2,9 @@ import { SortAsc } from "lucide-react";
 import Filter from "../../components/Filter";
 import ProductCard from "../../components/ProductCard";
 import { useEffect, useState } from "react";
+import ProductShimmer from "../../components/ProductShimmer";
 
-const Products = () => {
+const ProductListing = () => {
   const [productsData, setProductsData] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -24,7 +25,9 @@ const Products = () => {
       <Filter />
       <div className="w-full">
         <div className="flex justify-between px-3 py-5 items-center">
-          <p className="text-sm xl:text-xl">Showing {productsData.length} Products</p>
+          <p className="text-sm xl:text-xl">
+            Showing {productsData.length} Products
+          </p>
           <div className="flex  gap-1 border p-1">
             <p className="text-gray-400 flex items-center tracking-tighter gap-1">
               <SortAsc />
@@ -36,26 +39,31 @@ const Products = () => {
             </select>
           </div>
         </div>
-        <div className="w-full px-5 grid items-center gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-          {/* card */}
 
-          {productsData &&
-            productsData.length > 0 &&
-            productsData.map((item) => (
-              <ProductCard
-                key={item.id}
-                id={item.id}
-                thumbnail={item.thumbnail}
-                title={item.title}
-                price={item.price}
-                disPercentage={item.discountPercentage}
-                reviews={item.reviews}
-              />
-            ))}
-        </div>
+        {productsData.length === 0 ? (
+          <ProductShimmer />
+        ) : (
+          <div className="w-full px-5 grid items-center gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            {/* card */}
+
+            {productsData &&
+              productsData.length > 0 &&
+              productsData.map((item) => (
+                <ProductCard
+                  key={item.id}
+                  id={item.id}
+                  thumbnail={item.thumbnail}
+                  title={item.title}
+                  price={item.price}
+                  disPercentage={item.discountPercentage}
+                  reviews={item.reviews}
+                />
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default Products;
+export default ProductListing;
