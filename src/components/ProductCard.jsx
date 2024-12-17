9353fcd132a4d3d3df0d3d1edde802424a943edb
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-17 12:22:38 +0530

    Change product card ui

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,30 +10,33 @@ const ProductCard = ({
   reviews,
 }) => {
   return (
-    <Link to={`/products/${id}`}>
-      <div className="w-full  border bg-white   rounded-md">
-        <div className="w-full h-[250px]">
-          <img src={thumbnail} alt={title} className="w-full h-full" />
+    <div className="w-full  border bg-white   rounded-md py-3">
+      <Link to={`/products/${id}`} className="w-full h-[250px]">
+        <img src={thumbnail} alt={title} className="w-full h-full" />
+      </Link>
+      <div className="flex flex-col items-center gap-2">
+        <h3 className="text-sm font-semibold tracking-tighter">{title}</h3>
+        <div className="flex items-center   gap-1 ">
+          <Star size={20} color="gold " fill="gold" />
+          <Star size={20} color="gold " fill="gold" />
+          <Star size={20} color="gold " fill="gold" />
+          <Star size={20} color="gold " fill="gold" />
+          <span className="text-sm pl-1 text-gray-400"> {500} Reviews</span>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <h3 className="text-sm font-semibold tracking-tighter">{title}</h3>
-          <div className="flex items-center   gap-1 ">
-            <Star size={20} color="gold " fill="gold" />
-            <Star size={20} color="gold " fill="gold" />
-            <Star size={20} color="gold " fill="gold" />
-            <Star size={20} color="gold " fill="gold" />
-            <span className="text-sm pl-1 text-gray-400"> {500} Reviews</span>
-          </div>
 
-          <p>
-            $71 <span className="text-gray-400">$500</span>{" "}
-            <span className="font-semibold text-red-600 tracking-tighter">
-              50% off
-            </span>
-          </p>
-        </div>
+        <p>
+          $71 <span className="text-gray-400">$500</span>{" "}
+          <span className="font-semibold text-red-600 tracking-tighter">
+            50% off
+          </span>
+        </p>
       </div>
-    </Link>
+      <div className="px-2 py-1">
+        <button className="w-full  py-2 rounded-md bg-red-400 text-white   hover:bg-red-600  font-semibold   transition-all">
+          Add To Cart
+        </button>
+      </div>
+    </div>
   );
 };
 
