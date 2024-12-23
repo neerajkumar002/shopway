acb451c9f91ed65f42b7c900b1a076a0a944c2fa
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-23 21:08:33 +0530

    feat: add routing for product and admin page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeLayout from "./components/Layout";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
+import Home from "./pages/home/Home";
+import ProductDetails from "./components/ProductDetails";
+import ProductListing from "./pages/products/ProductListing";
+import AdminLayout from "./components/admin/AdminLayout";
+import AdminDashboard from "./pages/admin/Dashboard";
+import AdminProducts from "./pages/admin/Products";
+import Users from "./pages/admin/Users";
+import EditProduct from "./pages/admin/EditProduct";
+import AddProduct from "./pages/admin/AddProduct";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeLayout />} />
+        <Route path="/" element={<HomeLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="products/:id" element={<EditProduct />} />
+          <Route path="products/add" element={<AddProduct />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
