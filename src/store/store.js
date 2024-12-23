04846668e982e325042ef862180be01809fded69
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-23 21:19:06 +0530

    feat: add auth reducer to the redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore,   } from "@reduxjs/toolkit";
 import productReducer from "./products/product-slice/index";
+import authReducer from "./auth-slice/index";
+
 //create redux store and include slices
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    auth: authReducer,
   },
 });
