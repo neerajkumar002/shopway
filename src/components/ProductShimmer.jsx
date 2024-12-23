f9bcc547cb047d4743ebac1e7fd5fbd36cb9e720
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-23 11:38:16 +0530

    add ProductShimmer component for displaying multiple loading state

---
+++ src/components/ProductShimmer.jsx
@@ -0,0 +1,39 @@
+import CardShimmer from "./CardShimmer";
+
+const ProductShimmer = () => {
+  return (
+    <div className="w-full px-5 grid items-center gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+      <CardShimmer />
+    </div>
+  );
+};
+
+export default ProductShimmer;
