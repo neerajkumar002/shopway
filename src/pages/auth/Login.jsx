0fcd290b1916d0ea7adcb8bcbe84f5888249568b
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-27 17:51:16 +0530

    feat: implement react hook form for login and update input handling

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -3,21 +3,24 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
 import { loginUser } from "../../store/auth-slice";
+import { ToastContainer } from "react-toastify";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [formData, setFormData] = useState({ username: "", password: "" });
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const { errorMessage, user, isAuthenticated } = useSelector(
     (state) => state.auth,
   );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (formData.username === "" && formData.password === "") {
-      setError("Please Enter Username and Password");
-    } else {
-      dispatch(loginUser(formData));
+  //  react hook form
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      dispatch(loginUser(data));
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -36,18 +39,15 @@ const Login = () => {
       <div className="bg-white shadow-lg w-[400px]  sm:w-[500px]  py-8 rounded-lg ">
         <h3 className="text-3xl text-center border-b-2 py-3">Login</h3>
         <form
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-4 w-full px-12 py-2 "
         >
           <div>
             <input
-              type="text"
-              placeholder="Username"
+              type="email"
+              placeholder="email"
               className="w-full border-b-2 outline-none px-1 py-3 text-xl"
-              value={formData.username}
-              onChange={(e) =>
-                setFormData({ ...formData, username: e.target.value })
-              }
+              {...register("email", { required: true })}
             />
           </div>
           <div>
@@ -55,10 +55,7 @@ const Login = () => {
               type="password"
               placeholder="password"
               className="w-full border-b-2 outline-none px-1 py-3 text-xl"
-              value={formData.password}
-              onChange={(e) =>
-                setFormData({ ...formData, password: e.target.value })
-              }
+              {...register("password", { required: true })}
             />
           </div>
           <div>
@@ -76,6 +73,7 @@ const Login = () => {
           <p className="text-center text-red-500">{errorMessage}</p>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
